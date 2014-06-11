930ee4edb21ca41f2c8406364e3af3db10e57a71
Nicolas Burroni <nmburroni@gmail.com>
2014-06-11 12:11:26 -0300

    Comenzado el TP de los movimientos del caballo

--- Trabajo Practico Movimientos del Caballo/src/testers/Test.java
+++ Trabajo Practico Movimientos del Caballo/src/testers/Test.java
@@ -1,11 +1,16 @@
 package testers;
 
+import views.MovesView;
+
+import javax.swing.*;
+
 /**
  * @author Nicolas Burroni
  * @since 5/28/2014
  */
 public class Test {
 	public static void main(String[] args) {
-
+		new MovesView(e -> System.out.println("Clicked")).setVisible(true);
 	}
+
 }
--- Trabajo Practico Movimientos del Caballo/src/views/MovesView.java
+++ Trabajo Practico Movimientos del Caballo/src/views/MovesView.java
@@ -1,16 +1,87 @@
 package views;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
 
 /**
  * @author Nicolas Burroni
  * @since 5/30/2014
  */
-public class MovesView {
+public class MovesView extends JFrame{
 
+	private GridBagConstraints constraints;
 	private JPanel rootPanel;
 	private JButton nextButton;
 	private JPanel leftPanel;
 	private JPanel rightPanel;
 	private JPanel boardPanel;
+	private Tile[][] tileArray;
+
+	public MovesView(ActionListener nextActionListener){
+		super("Horse Moves");
+		setContentPane(rootPanel);
+		setMinimumSize(new Dimension(700, 500));
+		setLocationRelativeTo(null);
+		nextButton.addActionListener(nextActionListener);
+	}
+
+	private void createUIComponents() {
+		GridBagLayout centerLayout = new GridBagLayout();
+		constraints = new GridBagConstraints();
+		boardPanel = new JPanel(centerLayout);
+		centerLayout.setConstraints(boardPanel, constraints);
+		tileArray = new Tile[8][8];
+		createBoard();
+	}
+
+	/**
+	 * Creates the visual board by adding different buttons with their respective images.
+	 */
+	public void createBoard(){
+		for(int i = 1; i <= 8; i++){
+			for(int j = 1; j <= 8; j++){
+				placeTile(i, j);
+			}
+		}
+	}
+
+	/**
+	 * Places a button with its image on the given coordinate.
+	 * @param i i coordinate.
+	 * @param j j coordinate.
+	 */
+	private void placeTile(int i, int j){
+		Tile tile;
+		constraints.gridx = i;
+		constraints.gridy = j;
+		if((i % 2 == 0 && j % 2 != 0) || (i % 2 != 0 && j % 2 == 0)) tile = new Tile(false);
+		else tile = new Tile(true);
+		tileArray[j-1][i-1] = tile;
+		boardPanel.add(tile, constraints);
+	}
+
+	public void highlightPosition(int i, int j, int number){
+		tileArray[i][j].setText(String.valueOf(number));
+	}
+
+	public class Tile extends JPanel{
+
+		private JLabel label;
+
+		public Tile(boolean isWhite){
+			super();
+			label = new JLabel();
+			label.setOpaque(false);
+			label.setFont(new Font("Arial", Font.BOLD, 30));
+			setPreferredSize(new Dimension(40, 40));
+			if(isWhite) setBackground(Color.WHITE);
+			else setBackground(Color.BLACK);
+			add(label);
+		}
+
+		public void setText(String text){
+			label.setText(text);
+		}
+	}
 }
