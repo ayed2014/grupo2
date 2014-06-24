91126c700ad78d9495d7a7b590054cd10514b840
Nicolas Burroni <nmburroni@gmail.com>
2014-06-24 16:16:36 -0300

    Hecho el ejercicio 3

--- TP 10/Trabajo Practico 10 - Archivos de Texto/src/Ejercicio3.java
+++ TP 10/Trabajo Practico 10 - Archivos de Texto/src/Ejercicio3.java
@@ -1,3 +1,5 @@
+import java.io.*;
+
 /**
  * @author Nicolas Burroni
  * @since 6/18/2014
@@ -9,7 +11,36 @@ public class Ejercicio3 {
 	}
 
 	public static void menu(){
-
+		String path = Scanner.getString("Enter a file path: ", "Please enter a valid path! ");
+		FileReader fileReader = null;
+		while (true) {
+			try {
+				fileReader = new FileReader(path);
+				break;
+			} catch (FileNotFoundException e) {
+				path = Scanner.getString("File not found! Please enter another file path: ", "Please enter a valid path! ");
+			}
+		}
+		BufferedReader bufferedReader = new BufferedReader(fileReader);
+		BufferedWriter bufferedWriter;
+		int[] allowed = {1, 2};
+		int choice = Scanner.getInt("Convert to:\n1.\tLower case\n2.\tUpper case\n", "Please enter a valid choice! ", allowed);
+		try {
+			String line = bufferedReader.readLine();
+			if(choice == 1){
+				bufferedWriter = new BufferedWriter(new FileWriter(path + "(lowercase)"));
+				while(line != null){
+					bufferedWriter.write(line.toLowerCase() + "\n");
+				}
+			} else {
+				bufferedWriter = new BufferedWriter(new FileWriter(path + "(uppercase)"));
+				while(line != null){
+					bufferedWriter.write(line.toUpperCase() + "\n");
+				}
+			}
+		} catch (IOException e) {
+			System.out.println("Sorry, there was an error. Please try again.");
+		}
 	}
 
 }
