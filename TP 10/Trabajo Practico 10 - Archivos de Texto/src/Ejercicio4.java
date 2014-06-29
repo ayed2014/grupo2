dc40936ffd1c0d578a67a0420ff921b3c7297b22
Nicolas Burroni <nmburroni@gmail.com>
2014-06-29 18:53:19 -0300

    Terminado el TP 10. Hechos todos los TPs excepto los que se entregan para el final.

--- TP 10/Trabajo Practico 10 - Archivos de Texto/src/Ejercicio4.java
+++ TP 10/Trabajo Practico 10 - Archivos de Texto/src/Ejercicio4.java
@@ -1,15 +1,40 @@
+
+import java.io.*;
+
 /**
- * @author Nicolas Burroni
- * @since 6/18/2014
+ * Created by Francisco on 6/26/2014.
  */
 public class Ejercicio4 {
 
-	public static void main(String[] args) {
-		Ejercicio4.menu();
-	}
+    private FileReader fr;
+    private BufferedReader br;
+    private FileWriter fwGreater;
+    private FileWriter fwFewer;
 
-	public static void menu(){
+    public Ejercicio4(String file) throws IOException {
+        fr = new FileReader(file);
+        br = new BufferedReader(fr);
+    }
 
-	}
-
+    public void writeFiles() throws IOException {
+        String country;
+        double population;
+        double pbi;
+        fwFewer = new FileWriter("Countries with less than 30 million");
+        fwGreater = new FileWriter("Countries with more than or equal to 30 million");
+        country = br.readLine();
+        while (country != null){
+            population = Double.parseDouble(br.readLine());
+            pbi = Double.parseDouble(br.readLine());
+            if (population < 30000000){
+                fwFewer.write(country + "\n" + population + "\n" + pbi + "\n");
+            } else {
+                fwGreater.write(country + "\n" + population + "\n" + pbi + "\n");
+            }
+            country = br.readLine();
+        }
+        br.close();
+        fwFewer.close();
+        fwGreater.close();
+    }
 }
--- TP 10/Trabajo Practico 10 - Archivos de Texto/src/Ejercicio5.java
+++ TP 10/Trabajo Practico 10 - Archivos de Texto/src/Ejercicio5.java
@@ -1,15 +1,90 @@
+
+import java.io.*;
+import java.util.Scanner;
+
 /**
- * @author Nicolas Burroni
- * @since 6/18/2014
+ * @author Juan Ignacio Longo
+ * @date 28/06/14.
  */
 public class Ejercicio5 {
-
-	public static void main(String[] args) {
-		Ejercicio5.menu();
-	}
-
-	public static void menu(){
-
-	}
-
+    public void countryManager(String file,double population){
+        boolean pbi = false;
+        boolean population1 = false;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter PBI to save the field country and PBI");
+        System.out.println("Enter POB to save the field country and population");
+        System.out.println("Enter anything else to save every field");
+        String user = scanner.nextLine();
+        if(user.equals("PBI") || user.equals("pbi")){
+            pbi = true;
+        }
+        else if(user.equals("POB") || user.equals("pob")){
+            population1 = true;
+        }
+        else{
+            pbi = true;
+            population1 = true;
+        }
+        BufferedReader br = null;
+        BufferedWriter bw = null;
+        BufferedWriter bw2 = null;
+        try {
+            br = new BufferedReader(new FileReader(file));
+            bw = new BufferedWriter(new FileWriter("CountriesBelow"));
+            bw2 = new BufferedWriter(new FileWriter("CountriesAbove"));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        String lineCountry= "";
+        double linePopulation ;
+        double linePBI ;
+        try {
+            lineCountry = br.readLine();
+            linePopulation = Double.valueOf(br.readLine());
+            linePBI = Double.valueOf(br.readLine());
+            boolean firstTime = true;
+            while(lineCountry != null){
+                if(!firstTime){
+                    linePopulation = Double.valueOf(br.readLine());
+                    linePBI = Double.valueOf(br.readLine());
+                }
+                if(pbi && population1){
+                    if(linePopulation < population){
+                        bw.write(lineCountry + "\n");
+                        bw.write(String.valueOf(linePopulation) + "\n");
+                        bw.write(String.valueOf(linePBI) + "\n");
+                    } else {
+                        bw2.write(lineCountry + "\n");
+                        bw2.write(String.valueOf(linePopulation) + "\n");
+                        bw2.write(String.valueOf(linePBI) + "\n");
+                    }
+                }
+                else if(pbi){
+                    if(linePopulation < population){
+                        bw.write(lineCountry + "\n");
+                        bw.write(String.valueOf(linePBI) + "\n");
+                    } else {
+                        bw2.write(lineCountry + "\n");
+                        bw2.write(String.valueOf(linePBI) + "\n");
+                    }
+                }
+                else{
+                    if(linePopulation < population){
+                        bw.write(lineCountry + "\n");
+                        bw.write(String.valueOf(linePopulation) + "\n");
+                    } else {
+                        bw2.write(lineCountry + "\n");
+                        bw2.write(String.valueOf(linePopulation) + "\n");
+                    }
+                }
+                lineCountry = br.readLine();
+                firstTime = false;
+            }
+            br.close();
+            bw.close();
+            bw2.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
